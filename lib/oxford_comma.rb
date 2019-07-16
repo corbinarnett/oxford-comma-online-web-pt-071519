b57eeab3d963fc69b014725feae2e6dc6e07cbf6
corbinarnett <corbin.c.arnett@gmail.com>
2019-07-16 14:00:10 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ require 'pry'
 def oxford_comma(array)
     #binding.pry
   if array.length == 1
-    array.join(", ")
+    array.join
   elsif array.length == 2
     array.join(" and ")
   elsif array.length > 2
