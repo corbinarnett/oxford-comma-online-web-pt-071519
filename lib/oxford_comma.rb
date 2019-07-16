9fc25e3c5ad825ae2f45e8453cc5f45197d3700c
corbinarnett <corbin.c.arnett@gmail.com>
2019-07-16 13:53:19 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
+require 'pry'
 def oxford_comma(array)
-
+    #binding.pry
+  if array.length == 1
+    array.join(", ")
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length > 2
+    #selecting last element in array using.last
+    #.insert => inserts other_str before the character at the given index
+    # "abcd".insert(0, 'X')    #=> "Xabcd"
+    array.last.insert(0, "and ") 
+    #binding.pry
+    array.join(", ") 
+  end 
 end
